174b9d95b478b5c09012f2c7258c820aad2a3a76
ShrirangaB <77617450+ShrirangaB@users.noreply.github.com>
2021-01-19 19:06:39 +0530

    Add files via upload

---
+++ homePage.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+import 'getStarted.dart';
+
+class HomePageDoctor extends StatefulWidget {
+  @override
+  _HomePageDoctorState createState() => _HomePageDoctorState();
+}
+
+class _HomePageDoctorState extends State<HomePageDoctor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Health Care"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                'Hello,',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'How You can take care of yourself?',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Card(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.account_circle,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(
+                                  "Find Doctor",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => GetStarted(),
+                                    ),
+                                  );
+                                },
+                                subtitle: Text(
+                                  "210 Doctors",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(top: 20, left: 15),
+                              )
+                            ],
+                          ),
+                          width: 150,
+                          height: 150,
+                        ),
+                        color: Colors.cyan[400],
+                      ),
+                      Card(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.account_balance,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(
+                                  "Find Hospital",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  "30 Hospital",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(top: 20, left: 15),
+                              )
+                            ],
+                          ),
+                          width: 150,
+                          height: 150,
+                        ),
+                        color: Colors.black54,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Card(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.add_to_photos,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(
+                                  "Appointment",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  "59 Available",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(top: 20, left: 15),
+                              )
+                            ],
+                          ),
+                          width: 150,
+                          height: 150,
+                        ),
+                        color: Colors.black54,
+                      ),
+                      Card(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.calendar_today,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(
+                                  "DrugList",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  "22 Services",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(top: 20, left: 15),
+                              )
+                            ],
+                          ),
+                          width: 150,
+                          height: 150,
+                        ),
+                        color: Colors.black54,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(27),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(10),
+        color: Colors.white54,
+      ),
+    );
+  }
+}
---
+++ loginNew.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+class LoginNew extends StatefulWidget {
+  @override
+  _LoginNewState createState() => _LoginNewState();
+}
+
+class _LoginNewState extends State<LoginNew> {
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipper: WaveClipperTwo(flip: false),
+            child: Container(
+              child: Image.network(
+                "https://i.pinimg.com/originals/61/c7/a2/61c7a28bbb12ee7d75064e9dba23305e.png",
+                height: 290,
+                width: 380,
+              ),
+              height: 250,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            'Lets Get Started',
+            style: TextStyle(
+              color: Colors.orange[900],
+              fontSize: 25,
+            ),
+          ),
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(5),
+                border: InputBorder.none,
+                hintText: "name@email.com"),
+          ),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(5),
+              border: InputBorder.none,
+              hintText: "********",
+            ),
+          ),
+          MaterialButton(
+            minWidth: 350,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(color: Colors.orange[900])),
+            textColor: Colors.white,
+            color: Colors.orange[900],
+            padding: const EdgeInsets.all(8.0),
+            child: new Text(
+              "Login",
+            ),
+            onPressed: () {},
+          ),
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot Password?',
+              style: TextStyle(color: Colors.orange[900]),
+            ),
+          ),
+          MaterialButton(
+            minWidth: 350,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(color: Colors.black54)),
+            textColor: Colors.white,
+            color: Colors.black54,
+            padding: const EdgeInsets.all(8.0),
+            child: new Text(
+              "Sign Up",
+            ),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text('or'),
+          SizedBox(
+            height: 20,
+          ),
+          FloatingActionButton(
+            child: Text(
+              'f',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.orange[900],
+            onPressed: () {},
+          ),
+          Positioned(
+            bottom: 0.0,
+            child: ClipPath(
+              clipper: WaveClipperTwo(flip: false, reverse: true),
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.deepOrange[600],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
